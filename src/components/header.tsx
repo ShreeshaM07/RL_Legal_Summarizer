3faf9fe11e560720e156537e5cbe9bd7324dc766
ShreeshaM07 <shreesham2k22@yahoo.com>
2025-04-23 13:51:03 +0530

    Linting for responsive design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import Link from 'next/link'
 
 export default function Header(){
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -8,7 +9,7 @@ export default function Header(){
     return(
         <header className="bg-white">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-            <a className="block text-teal-600" href="/">
+            <Link className="block text-teal-600" href="/">
             <span className="sr-only">Home</span>
             <svg className="h-8" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -16,8 +17,7 @@ export default function Header(){
                 fill="currentColor"
                 />
             </svg>
-            </a>
-
+            </Link>
             <div className="flex flex-1 items-center justify-end md:justify-between">
             <nav aria-label="Global" className="hidden md:block">
                 <ul className="flex items-center gap-6 text-sm">
